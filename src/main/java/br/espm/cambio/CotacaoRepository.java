3a8227e6e7fd115e9ae40955c1bc87a73dbe8ea0
Henrique Frata <henrique.frata360@gmail.com>
2022-06-03 11:23:56 -0300

    get cotacao.1

--- src/main/java/br/espm/cambio/CotacaoRepository.java
+++ src/main/java/br/espm/cambio/CotacaoRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface CotacaoRepository extends CrudRepository <CotacaoModel , String>{
 
-    @Override
-    Iterable<CotacaoModel> findAll();
-    
     @Query ("Select m from CotacaoModel m WHERE UPPER(m.idMoeda) = UPPER(:idMoeda)")
     Iterable<CotacaoModel> findAll (@Param ("idMoeda") String idMoeda);
 }
